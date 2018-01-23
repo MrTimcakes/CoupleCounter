1fddae85a4514cc2988271f77eecc67b51f3d676
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-23 18:39:22 +0000

    Only BLE If BTN Active

--- CoupleCounter.ino
+++ CoupleCounter.ino
@@ -6,6 +6,25 @@
 #define SERVICE_UUID             "3efad57b-7510-46b9-ad7d-6a4411b66a53" // Generic CoupleCounter Service
 #define TIME_CHARACTERISTIC_UUID "8852eb13-b7d2-4bca-a8a7-0ed21bc1d0c8" // Current Time Characteristic
 
+BLECharacteristic *pTimeCharacteristic; // Global So I can Updated It 
+
+void setup() {
+  pinMode(0, INPUT);
+  Serial.begin(115200);
+  Serial.println(esp_sleep_get_wakeup_cause());
+
+  if(!digitalRead(0)){ // If Boot BTN Pressed, start BLE
+    BLESetup();
+  }
+  
+  Serial.println("Sleeping!");
+  esp_sleep_enable_timer_wakeup(10 * 1000000);
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  // Never Executed
+}
 
 class TimeCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
@@ -17,28 +36,20 @@ class TimeCallbacks: public BLECharacteristicCallbacks {
     }
 };
 
-void setup() {
-  Serial.begin(115200);
-
+void BLESetup(){
   BLEDevice::init("CoupleCounter");
   BLEServer *pServer = BLEDevice::createServer();
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  BLECharacteristic *pTimeCharacteristic = pService->createCharacteristic(
-                                         TIME_CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
+  BLECharacteristic *pTimeCharacteristic = pService->createCharacteristic(TIME_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
   pTimeCharacteristic->setCallbacks(new TimeCallbacks());
   pTimeCharacteristic->setValue("Initial Value");
   pService->start();
-  
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
+
+  while(!digitalRead(0)){ // While Boot BTN Pressed
+    delay(1000);
+  }
 }
 
-void loop() {
-  delay(10000);
-}
+
