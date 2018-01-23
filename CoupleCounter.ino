8dbe00b5083e84d05d1459e1e7235b6bee95beac
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-23 03:24:01 +0000

    Basic Custom BLE Service + Characteristic

--- CoupleCounter.ino
+++ CoupleCounter.ino
@@ -1,9 +1,47 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-void setup(){
+// Version 4 UUIDs (Basically Random)
+#define SERVICE_UUID             "3efad57b-7510-46b9-ad7d-6a4411b66a53" // Generic CoupleCounter Service
+#define TIME_CHARACTERISTIC_UUID "8852eb13-b7d2-4bca-a8a7-0ed21bc1d0c8" // Current Time Characteristic
+
+
+class TimeCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++){
+          Serial.print(value[i]);
+        }
+        Serial.println();
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  BLEDevice::init("CoupleCounter");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pTimeCharacteristic = pService->createCharacteristic(
+                                         TIME_CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pTimeCharacteristic->setCallbacks(new TimeCallbacks());
+  pTimeCharacteristic->setValue("Initial Value");
+  pService->start();
   
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
 
-void loop(){
-  
+void loop() {
+  delay(10000);
 }
-
