82f1d0e1fe5abc6eccd252871a3790dc3957280b
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-25 02:55:36 +0000

    Basic Printing

--- CoupleCounter.ino
+++ CoupleCounter.ino
@@ -1,6 +1,16 @@
+#define arr_len( x )  ( sizeof( x ) / sizeof( *x ) )
+
+#include "Phrases.h"
 
 void setup(){
-  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  int EventID = random( arr_len(EventTimes) );
+  Serial.println(Aliases[ random(arr_len(Aliases)) ]); // Random Alias
+  Serial.println(EventTimes[EventID]); // Time of Event
+  Serial.println(EventDesc[EventID]); // Event Description
 }
 
 void loop(){
