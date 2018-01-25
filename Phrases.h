068b5b54dba586e7d5395108186e197a0227eecb
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-25 02:52:59 +0000

    Filler Event Data

---
+++ Phrases.h
@@ -0,0 +1,4 @@
+String   Aliases[]    = { "Alias0", "Alias1", "Alias3" };
+uint32_t EventTimes[] = { 1514764800, 1515542400 };
+String   EventDesc[]  = { "It's been [x] since we Event0",
+                          "It's been [x] sinse we Event1" };
