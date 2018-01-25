97dd824cf4b4f89df98b2473d888a24265be7db1
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-25 04:01:56 +0000

    Display on OLED

--- CoupleCounter.ino
+++ CoupleCounter.ino
@@ -2,15 +2,22 @@
 
 #include "Phrases.h"
 
-void setup(){
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+#include "SSD1306.h"
+SSD1306  display(0x3c, 4, 5); // SDA, SCL    NodeMCU: D2, D1
 
+
+void setup(){
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  
   int EventID = random( arr_len(EventTimes) );
-  Serial.println(Aliases[ random(arr_len(Aliases)) ]); // Random Alias
-  Serial.println(EventTimes[EventID]); // Time of Event
-  Serial.println(EventDesc[EventID]); // Event Description
+  String Message = EventDesc[EventID];
+  Message.replace("[x]", "TIMESINCE");
+  display.drawString(0, 0, Aliases[random(arr_len(Aliases))] + ",");
+  display.drawStringMaxWidth( 0, 10, 128, Message );
+  display.display();
 }
 
 void loop(){
