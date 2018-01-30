17b5f68894bcb4e96691dd6abdc971bc105fd3e1
Timothy Adamson <MrTimcakes@gmail.com>
2018-01-30 02:28:17 +0000

    Milestone: Days since on OLED

--- CoupleCounter.ino
+++ CoupleCounter.ino
@@ -5,10 +5,20 @@
 #include "SSD1306.h"
 SSD1306  display(0x3c, 4, 5); // SDA, SCL    NodeMCU: D2, D1
 
+#include "RTClib.h"
+RTC_DS1307 rtc;
+//RTC_DS3231 rtc;
 
 void setup(){
+  Serial.begin(115200);
   DisplayInit();
-  DisplayPhrase();
+
+  if (! rtc.begin()) {
+    DisplayRTCError();
+  }else{
+    DisplayPhrase();
+  }
+  
 
   ESP.deepSleep(3600e6);
 }
@@ -24,10 +34,19 @@ void DisplayInit(){
   display.setTextAlignment(TEXT_ALIGN_LEFT);
 }
 
+void DisplayRTCError(){
+  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+  display.drawString(64, 32, "NO RTC DETECTED");
+  display.display();
+}
+
 void DisplayPhrase(){
   int EventID = random( arr_len(EventTimes) );
   String Message = EventDesc[EventID];
-  Message.replace("[x]", "TIMESINCE");
+  
+  DateTime now = rtc.now();
+  Message.replace("[x]", String((now.unixtime() - EventTimes[EventID]) / 86400) + " Days");
+  
   display.drawString(0, 0, Aliases[random(arr_len(Aliases))] + ",");
   display.drawStringMaxWidth( 0, 10, 128, Message );
   display.display();
